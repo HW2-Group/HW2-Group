9d08ead0cc5269b859b43211df7c338afa407a35
BrettCarter <brettcartersud@gmail.com>
2015-06-02 21:13:53 -0600

    changed to 11

--- src/HW2.java
+++ src/HW2.java
@@ -8,7 +8,7 @@ public class HW2
     public static void main(String[] args)
     {
     	int x = 0;
-    	while(x < 10)
+    	while(x < 11)
     	{
 	        String student1 = "Student1: ";
 	        String student1Response[] = {"I like pie.", "Hello, my name is", "Java's got nothing on me", "OOP is not always the solution", "Blastoff! (straight from the book, here)", "Floating point errors are completely avoidable."};
