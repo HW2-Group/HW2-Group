f59085d106ad00a776dd75971a145b540f51e44c
BrettCarter <brettcartersud@gmail.com>
2015-06-02 20:49:45 -0600

    Changed while loop to not be infinite.

--- src/HW2.java
+++ src/HW2.java
@@ -2,11 +2,13 @@ import java.io.Console;
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class HW2
 {
     public static void main(String[] args)
     {
-    	while(true)
+    	int x = 0;
+    	while(x < 10)
     	{
 	        String student1 = "Student1: ";
 	        String student1Response[] = {"I like pie.", "Hello, my name is", "Java's got nothing on me", "OOP is not always the solution", "Blastoff! (straight from the book, here)", "Floating point errors are completely avoidable."};
@@ -14,7 +16,13 @@ public class HW2
 	
 	        Random r = new Random();
 	        System.out.println(student1 + student1Response[r.nextInt(6)]);
+	        x++;
     	}
 
     }
+}
+
+public class student
+{
+	
 }
